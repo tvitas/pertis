ec4d826462b51f2dacc1758617be2bb6f803d7a6
tvitas <vitalijus.trainys@lsmuni.lt>
2018-06-27 12:17:27 +0300

    Init

--- src/replace.php
+++
@@ -1,9 +0,0 @@
-<?php
-$row = array('task_id' => 7, 'task_reg_id' => 9);
-$query = "SELECT bun from bum WHERE num = %task_id% and fum = %task_reg_id%";
-echo "$query <br />";
-foreach ($row as $key => $value) {
-	$query = str_replace("%$key%", "$value", $query);
-}
-echo "$query<br />";
-?>
